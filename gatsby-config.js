b8679c97a06bf0c23007773cfe0044984a23fe3e
kokozeng <zwj.selina@qq.com>
2020-05-21 13:45:53 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/a12c985b8ddb441fbd1bab6d37c36837?v=4064a8b1dcbc4ba4a8b7d0044e03eeaf"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/f1a2e66ac7d74823b4a2c25761e2b07c?v=8a80d44ecbf34d2dbd333b34fc32b47c"
             }
         }
     ],
